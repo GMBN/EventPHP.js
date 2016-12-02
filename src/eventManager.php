8d0b1786eafd709899abdfbe74e7e6137223d023
Gian Nunes <gian@btitecnologia.com>
2016-12-02 08:58:42 -0200

    Update eventManager.php

--- src/eventManager.php
+++ src/eventManager.php
@@ -1,20 +1,20 @@
 <?php
  
    $e = new Event();
-    if (isset($_POST['event'])) {
+ if (isset($_POST['event'])) {
 
         $param = false;
         if (isset($_POST['param'])) {
             $param = $_POST['param'];
         }
-        if (isset($_POST['param'])) {
-            foreach ($_POST['context'] as $c) {
-                $file = 'app/event/' . $c . '.php';
-                if (file_exists($file)) {
-                    require $file;//inclui o arquivo de contexto
-                }
+        foreach ($_POST['context'] as $c) {
+
+            $c = str_replace(['..', '/'], '', $c); //evita a manipulacao de diretorio
+
+            $file = 'app/event/' . $c . '.php';
+            if (file_exists($file)) {
+                require $file; //inclui o arquivo de contexto
             }
         }
-        $e->trigger($_POST['event'], $param);//aciona o evento solicitado
+        $e->trigger($_POST['event'], $param); //aciona o evento solicitado
     }
-}
